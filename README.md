d28d2764b640a2d976c5d98bd43b08d609c1ebcb
Gaxalco <gatien.penchaud@etu.u-bordeaux.fr>
2024-10-23 18:18:13 +0200

    commit

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Checkers
 
 A simple checker game made with SDL2
+https://lazyfoo.net/tutorials/SDL/02_getting_an_image_on_the_screen/index.php
 
 Under construction I guess
--- main.c
+++ main.c
@@ -1,87 +1,84 @@
 #include "constants.h"
 
 
-/* gcc main.c -o main -Wall -std=c99 -lSDL2 ; ./main */
+/* gcc main.c -g -o main -Wall -std=c99 -lSDL2 ; ./main */
+
 
 //Starts up SDL and creates window
 bool init();
 
 //Loads media
-bool loadMedia();
+bool loadMedia() {
+    // Placeholder function, returns true for now
+    return true;
+}
 
 //Frees media and shuts down SDL
 void close();
 
-App game;
-game.window = NULL;
-game.screen = NULL;
+App game = {NULL, NULL};
 
 bool init() {
     //Initialization flag
     bool success = true;
 
     //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
         printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
         success = false;
-    }
-    else
-    {
+    } else {
         //Create window
-        game.window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN );
-        if( game.window == NULL )
-        {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        game.window = SDL_CreateWindow("Checkers", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+        if (game.window == NULL) {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
             success = false;
-        }
-        else
-        {
+        } else {
             //Get window surface
-            game.screen = SDL_GetWindowSurface( game.window );
+            game.screen = SDL_GetWindowSurface(game.window);
         }
     }
 
     return success;
 }
 
-int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        printf("SDL_Init Error: %s\n", SDL_GetError());
-        return 1;
-    }
 
-    
 
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-    } else {
-        game.window = SDL_CreateWindow("Checkers", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-        if( game.window == NULL )
-        {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-        } else {
-            // Set up the screen
-            game.screen = SDL_GetWindowSurface(game.window);
-
-            // Fill the screen white
-            SDL_FillRect(game.screen, NULL, SDL_MapRGB(game.screen->format, 0xFF, 0xFF, 0xFF));
-
-            // Update the window
-            SDL_UpdateWindowSurface(game.window);
-
-            //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
-        }
+void close() {
+    //Deallocate surface
+    if (game.screen != NULL) {
+        SDL_FreeSurface(game.screen);
+        game.screen = NULL;
     }
 
     //Destroy window
-    SDL_DestroyWindow( game.window );
+    if (game.window != NULL) {
+        SDL_DestroyWindow(game.window);
+        game.window = NULL;
+    }
 
     //Quit SDL subsystems
     SDL_Quit();
+}
 
+
+
+int main(int argc, char* argv[]) {
+    
+    if (!init()) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    } else {
+        // Fill the screen white
+        // SDL_FillRect(game.screen, NULL, SDL_MapRGB(game.screen->format, 0xFF, 0xFF, 0xFF));
+
+        // // Update the window
+        // SDL_UpdateWindowSurface(game.window);
+
+        // //Hack to get window to stay up
+        // SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+    }
+
+        
 
     return 0;
 }
